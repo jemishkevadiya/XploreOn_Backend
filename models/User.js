ce43c7dd464d804925818263054044231ed1c063
jemish kevadiya <jemishkevadiya007@gmail.com>
2024-12-17 17:13:54 -0500

    Create User Schema

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        match: [
+            /^\S+@\S+\.\S+$/,
+            'Please use a valid email address',
+        ],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        select: false,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+},
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model('User', userSchema);
