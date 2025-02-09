f79462144be8b6b87b08b1cbf5f260f53271fc90
jemish kevadiya <jemishkevadiya007@gmail.com>
2025-02-08 20:31:21 -0500

    update in server.js

--- server.js
+++ server.js
@@ -11,23 +11,18 @@ const carRentalRoutes = require('./routes/carRentalRoutes');
 const hotelRoutes = require('./routes/hotelRoutes');
 const app = express();
 
-// Load Firebase credentials
 const credentials = JSON.parse(
   fs.readFileSync('./credentials.json')
 );
 
-// Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(credentials)
 });
 
-// Middleware to parse JSON
 app.use(express.json());
 
-// Connect to the database
 connectDB();
 
-// Routes
 app.use('/api/user', userRoutes);
 
 app.use(async function(req, res, next) {
@@ -47,7 +42,7 @@ app.use('/api/car_rental', carRentalRoutes);
 app.use('/api/hotels', hotelRoutes);
 
 // Start the server
-const PORT = process.env.SERVER_PORT; // Fallback to 3000 if SERVER_PORT is not set
+const PORT = process.env.SERVER_PORT; 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
