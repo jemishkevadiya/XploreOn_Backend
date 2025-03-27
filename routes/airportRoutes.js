92226e09f0fce6e7d1c773d8d19e58947c4b9f0a
jemish kevadiya <jemishkevadiya007@gmail.com>
2025-03-27 15:01:47 -0400

    changes made in airport code

--- routes/airportRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAirportsByCity } = require("../controllers/AirportController");
-
-router.get("/search", getAirportsByCity);
-
-module.exports = router;
